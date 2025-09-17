6715a976c09b62866d121783bc737edbda747fd6
Adil-Omar <madilomar1@yahoo.com>
2025-09-17 11:28:06 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import Privacy from './pages/Privacy'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Privacy from "./pages/Privacy";
+import Privacy2 from "./pages/Privacy2"; // create this new page
 
 export default function App() {
   return (
-    <div>
-      <Privacy />
-    </div>
-  )
+    <Router>
+      <Routes>
+        {/* Default page */}
+        <Route path="/" element={<Privacy />} />
+
+        {/* Second page */}
+        <Route path="/AI-Photo-Enhancer" element={<Privacy2 />} />
+      </Routes>
+    </Router>
+  );
 }
