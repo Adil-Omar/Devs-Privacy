8617fa02f309bdc5808a9ff015b44b5692eb2436
Adil-Omar <madilomar1@yahoo.com>
2025-10-01 10:00:19 +0500

    added a page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Privacy from "./pages/Privacy";
 import Privacy2 from "./pages/Privacy2"; // create this new page
+import Privacy3 from "./pages/Privacy3";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
 
         {/* Second page */}
         <Route path="/AI-Photo-Enhancer" element={<Privacy2 />} />
+        <Route path="/NovaChat-AI" element={<Privacy3 />} />
       </Routes>
     </Router>
   );
